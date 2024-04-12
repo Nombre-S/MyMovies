f71d762f2694c4d831bc7e8662d988f64b2de6c0
Nombre-S <78140736+Nombre-S@users.noreply.github.com>
2024-04-12 05:43:43 +0000

    Initial commit

--- mymovies/movies/models.py
+++ mymovies/movies/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Genre(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
