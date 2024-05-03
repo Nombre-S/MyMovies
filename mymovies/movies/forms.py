1537014263b0f22ff4837a67d62779bf2e42d858
Nombre-S <78140736+Nombre-S@users.noreply.github.com>
2024-05-03 14:47:42 -0700

    changed name form style

--- mymovies/movies/forms.py
+++ mymovies/movies/forms.py
@@ -1,4 +1,7 @@
+from typing import Required
 from django import forms
 
 class NameForm(forms.Form):
-    your_name = forms.CharField(label="Your name", max_length=100)
+    your_name = forms.CharField(label="Nombre:", max_length=100, help_text="100 Char Max", 
+                                error_messages={"required": "El nombre es requerido"},
+                                widget=forms.Textarea(attrs={"class":"text-gray-400", "rows": 3, "cols": 60}))
