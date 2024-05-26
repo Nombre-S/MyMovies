98601c1f8f54dfdec5a5b73f77b7b04c9dceb5af
Nombre-S <78140736+Nombre-S@users.noreply.github.com>
2024-05-25 23:44:17 -0700

    Add review form

--- mymovies/movies/forms.py
+++ mymovies/movies/forms.py
@@ -1,12 +1,17 @@
-from typing import Required
 from django import forms
+from .models import MovieReview
 
 class NameForm(forms.Form):
     your_name = forms.CharField(label="Nombre:", max_length=100, help_text="100 Char Max", 
                                 error_messages={"required": "El nombre es requerido"},
                                 widget=forms.Textarea(attrs={"class":"text-gray-400", "rows": 3, "cols": 60}))
 
-class MovieReviewForm(forms.Form):
-    movie_id = forms.IntegerField(widget=forms.HiddenInput())
-    rating = forms.IntegerField(label='Rating', min_value=1, max_value=100)
-    description = forms.CharField(label='Description', widget=forms.Textarea)
+class MovieReviewForm(forms.ModelForm):
+    class Meta:
+        model = MovieReview
+        fields = ['rating', 'review']
+        widgets = {
+            'rating': forms.NumberInput(attrs={'min': 1, 'max': 100, 'required': True}),
+            'review': forms.Textarea(attrs={'rows': 4, 'cols': 50}),
+        }
+
